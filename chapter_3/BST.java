8e89fde08ab356351ddd129193b111048d0a721a
Rudy Zhang <425220727@qq.com>
2016-11-01 15:01:27 +0800

    Update BST.java

--- chapter_3/BST.java
+++ chapter_3/BST.java
@@ -48,4 +48,116 @@ public class BST<Key extends Comparable<Key>, Value> {
         x.N = size(x.left) + size(x.right) + 1;
         return x;  
     }
+
+    public Key min() {
+        return min(root).key;
+    } 
+
+    private Node min (Node x) {
+        if(x.left == null) {
+            return x;
+        }
+        return min(x, left);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if(x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if(x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp == 0) return x;
+        if(cmp < 0) return floor(x.left, key);
+        Node t = floor(x.rigth, key);
+        if(t != null) return null;
+        else return x;
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    private Node select(Node x, int k) {
+        if(x == null) return null;
+        int t = size(x.left);
+        if(t > k) {
+            return select(x.left, k);
+        } else if(t < k) {
+            return select(x.rigth, k-t-1);
+        } else {
+            return x;
+        }
+    }
+
+    public int rank(Key, key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key, key, Node x) {
+        if(x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) return rank(key, left);
+        else if(cmp > 0) return 1+size(x.left) + rank(key, x.right);
+        else return size(x.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if(x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(Key, key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if(x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) {
+            x.left = delete(x.left, key);
+        } else if(cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            if(x.rigth == null) return x.left;
+            if(x.left == null) return x.right;
+            Node t = x;
+            x = min(t.rigth);
+            x.rigth = deleteMin(t.rigth);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }    
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue; 
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if(x == null) return null;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if(cmplo < 0) keys(x.left, queue, lo, hi);
+        if(cmolo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if(cmphi > 0) keys(x.rigth, queue, lo, hi);
+    }
 }
