3f28bef5fdebe3be0c219e3ff0a28ef7c65dbc40
He Zhang (Rudy) <425220727@qq.com>
2016-11-04 16:09:06 +0800

    Create DepthFirstPaths.java

---
+++ chapter_4/DepthFirstPaths.java
@@ -0,0 +1,37 @@
+public class DepthFirstPaths {
+
+    private boolean[] marked;
+    private int[] edgeTo;//从起点到一个顶点的已知路径上的最后一个顶点
+    private final int s;//起点
+
+    public DepthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        dfs(G, s);
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        for(int w : G.adj(v)) {
+            if(!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if(!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        for(int x = v; x != s; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+}
