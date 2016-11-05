ca21ff27c25b06c8ae95acf119b61ff5d6ad2194
He Zhang (Rudy) <425220727@qq.com>
2016-11-05 15:22:28 +0800

    Create Digraph.java

---
+++ chapter_4/Digraph.java
@@ -0,0 +1,42 @@
+public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for(int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for(int v = 0; v < V; v++) {
+            for(int w : adj(v)) {
+                R.addEdge(w, v);
+            }
+        }
+
+        return R;
+    }
+}
