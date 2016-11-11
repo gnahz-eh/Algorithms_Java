e3bb37e4946ea5142410e195f88744e4d6029c32
He Zhang (Rudy) <425220727@qq.com>
2016-11-11 17:20:20 +0800

    Update ViolentSearchS.java

--- chapter_5/ViolentSearchS.java
+++ chapter_5/ViolentSearchS.java
@@ -14,3 +14,24 @@ public static int search(String pat, String txt) {
     }
     return N;
 }
+
+
+public static int search(String pat, String txt) {
+    int j, M = pat.length();
+    int i, N = txt.length();
+    
+    for(i = 0, j = 0; i < N && j < M; i++) {
+        if(txt.charAt(i) == pat.charAt(j)) {
+            j++;
+        } else {
+            i -= j;
+            j = 0;
+        }    
+    }
+    
+    if(j == M) {
+        return i - M;
+    } else {
+        return N;
+    }
+}
